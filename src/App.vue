2c6d165375e68886b0911e1bebe59f869de02644
Dgen <parshukove@yandex.ru>
2021-10-01 13:26:28 +0300

    fetch employees

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,36 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, onMounted } from "vue";
 import HelloWorld from "./components/HelloWorld.vue";
+import employeeComposition from "@/compositions/employeeComposition";
 
 export default defineComponent({
   name: "App",
   components: {
     HelloWorld,
+  },
+  setup() {
+    /**
+     * Compositions
+     */
+    const employeesManagement = employeeComposition();
+
+    /**
+     * Hooks
+     */
+    onMounted(async () => {
+      await getEmployees();
+    });
+
+    /**
+     * Methods
+     */
+    const getEmployees: () => void = async () => {
+      const data = await employeesManagement.getEmployees();
+
+      console.log(data);
+    };
   },
 });
 </script>
