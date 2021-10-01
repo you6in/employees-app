e39ea26c2281d05b33ec08816044ec66c14509a7
Dgen <parshukove@yandex.ru>
2021-10-01 13:21:30 +0300

    add employee types

---
+++ src/types/employees.d.ts
@@ -0,0 +1,11 @@
+export interface Employee {
+  id: string;
+  name: string;
+  avatar: string;
+  email: string;
+  phone: string;
+  employeeCode: string;
+  position: string;
+  joinDate: string;
+  createdAt: string;
+}
